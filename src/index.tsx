d4569b9eefd96b2febffe263a1b21a3a72f1d17a
guabaichi <leon.antonio.rodriguez.sanpere@gmail.com>
2023-12-13 15:32:56 +0700

    Part 4 done.

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+// import './index.css';
 import GoalApp from './goalComponents/GoalApp';
+import App from './App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <GoalApp />
+  <div>
+    {/* <GoalApp /> */}
+    <App />
+  </div>
 );
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-import App from './goalComponents/GoalApp'
+import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
   </React.StrictMode>,
 )
