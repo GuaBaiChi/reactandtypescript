6df16171ffae7c71acef449cbcd0168e55a3d7aa
guabaichi <leon.antonio.rodriguez.sanpere@gmail.com>
2023-12-17 12:06:19 +0700

    Redux Project set up

---
+++ src/apps/redux/ReduxApp.tsx
@@ -0,0 +1,21 @@
+import Header from './reduxComponents/Header';
+import Shop from './reduxComponents/Shop';
+import Product from './reduxComponents/Product';
+import { DUMMY_PRODUCTS } from './dummy-products';
+
+function ReduxApp() {
+  return (
+    <>
+      <Header />
+      <Shop>
+        {DUMMY_PRODUCTS.map((product) => (
+          <li key={product.id}>
+            <Product {...product} />
+          </li>
+        ))}
+      </Shop>
+    </>
+  );
+}
+
+export default ReduxApp;
