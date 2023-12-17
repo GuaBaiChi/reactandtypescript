a33d722130b7472df33c9b7b59d9fad7fc4f5202
guabaichi <leon.antonio.rodriguez.sanpere@gmail.com>
2023-12-17 10:11:10 +0700

    Timers app fixed

--- src/apps/timer/timerComponents/Timer.tsx
+++ src/apps/timer/timerComponents/Timer.tsx
@@ -1,30 +1,35 @@
 import { useEffect, useRef, useState } from 'react';
+
 import Container from './UI/Container';
 import { useTimersContext, type Timer as TimerProps } from '../store/timers-context';
 
 export default function Timer({ name, duration }: TimerProps) {
-  const interval = useRef<number | NodeJS.Timeout | null>(null);
+  const interval = useRef<ReturnType<typeof setInterval> | null>(null);
   const [remainingTime, setRemainingTime] = useState(duration * 1000);
   const { isRunning } = useTimersContext();
 
-  if (remainingTime <= 0 && interval.current !== null) {
-    if (typeof interval.current !== 'number') {
-      clearInterval(interval.current);
-    }
+  if (remainingTime <= 0 && interval.current) {
+    clearInterval(interval.current);
   }
 
   useEffect(() => {
-    let timer: NodeJS.Timeout;
+    let timer: ReturnType<typeof setInterval>;
 
     if (isRunning) {
       timer = setInterval(() => {
-        setRemainingTime((prevTime) => prevTime - 50);
+        if (remainingTime < 50 && remainingTime > 0) {
+          // If between 0 - 50 just set to 0
+          setRemainingTime((prevTime) => 0);
+        } else if (remainingTime <= 0) {
+          // Do nothing... Don't go negative
+        } else {
+          // Adjust timer by 5ms
+          setRemainingTime((prevTime) => prevTime - 50);
+        }
       }, 50);
       interval.current = timer;
-    } else if (interval.current !== null) {
-      if (typeof interval.current !== 'number') {
-        clearInterval(interval.current);
-      }
+    } else if (interval.current) {
+      clearInterval(interval.current)
     }
 
     return () => clearInterval(timer);
